4360b5cd83bebeab22cb151272766d1af860053d
iuc73663 <ivan5247@gmail.com>
2020-06-08 11:45:38 -0700

    updated resume

--- In the works/fractals/sketch.js
+++ In the works/fractals/sketch.js
@@ -1,15 +1,12 @@
-var angle = 0;
 var slider;
 let button;
 
 bgColor = "#808080"
 function setup() {
   createCanvas(400, 400);
-  slider = createSlider(0, TWO_PI, PI / 4, 0.01);
-  slider.position(80,420);
+  //slider = createSlider(0, TWO_PI, PI / 4, 0.01);
+  //slider.position(80,420);
 
-  colorPicker = createColorPicker(bgColor);
-  colorPicker.position(420, 0);
 
   button = createButton('Reset');
   button.position(10, 420);
@@ -18,31 +15,18 @@ function setup() {
 
 function reset()
 {
-  angle = PI / 4;
-  slider.value(angle);
+  //slider.value(angle);
   redraw();
 }
 
 function draw() {
-  bgColor = colorPicker.color()
   background(bgColor);
-  angle = slider.value();
+  ///angle = slider.value();
   stroke(255);
-  translate(200, height);
-  branch(100);
+  drawSquare(10,10,20);
+  drawSquare(10,20,20);
 }
 
-function branch(len) {
-  line(0, 0, 0, -len);
-  translate(0, -len);
-  if (len > 3) {
-    push();
-    rotate(angle);
-    branch(len * 0.67);
-    pop();
-    push();
-    rotate(-angle);
-    branch(len * 0.5);
-    pop();
-  }
+function drawSquare(x,y,len) {
+  square(x, y, len);
 }
